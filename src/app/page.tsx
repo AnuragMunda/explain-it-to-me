65e705d501d627b0a9d15dc43240a8aba60523cb
AnuragMunda <anurag.munda005@gmail.com>
2025-07-20 23:29:43 +0530

    add sign in with next auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,30 @@ import Footer from "@/components/footer";
 import InfinitePromptScroll from "@/components/Infinte-prompt-scroll";
 import PromptInput from "@/components/prompt-input";
 import ResultSection from "@/components/result-section";
+import { signIn } from "@/lib/auth";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-linear-to-b from-[#093b45] to-black text-white flex flex-col px-4 py-5 overflow-y-scroll">
       <header className="mb-25">
-        <div className="text-lg md:text-xl tracking-wider">
-          <span className="font-extrabold">E</span>
-          <span className="font-light">xplain</span>
-          <span className="font-extrabold">I</span>
-          <span className="font-light">t</span>
-          <span className="font-extrabold">T</span>
-          <span className="font-light">o</span>
-          <span className="font-extrabold">M</span>
-          <span className="font-light">e</span>
+        <div className="flex justify-between items-center">
+          <div className="text-lg md:text-xl tracking-wider">
+            <span className="font-extrabold">E</span>
+            <span className="font-light">xplain</span>
+            <span className="font-extrabold">I</span>
+            <span className="font-light">t</span>
+            <span className="font-extrabold">T</span>
+            <span className="font-light">o</span>
+            <span className="font-extrabold">M</span>
+            <span className="font-light">e</span>
+          </div>
+
+          <button className="cursor-pointer hover:bg-[#1493ac] px-3 py-2 transition-all ease-in-out rounded-lg" onClick={async () => {
+            'use server'
+            await signIn("google")
+          }}>
+            Sign In
+          </button>
         </div>
       </header>
 
@@ -24,7 +34,7 @@ export default function Home() {
       <InfinitePromptScroll />
 
       <ResultSection />
-      
+
       <Footer />
     </main >
   );
--- src/components/prompt-input.tsx
+++ src/components/prompt-input.tsx
@@ -61,7 +61,7 @@ const PromptInput: React.FC = () => {
                     >
                         <h1 className="font-semibold text-4xl md:text-5xl tracking-wide">What can I explain?</h1>
                         <div className="flex flex-col items-center gap-7 w-full">
-                            <div className="w-full rounded-2xl border-[#1493ac] bg-black flex justify-between md:w-[80%] lg:w-[65%] min-h-40 max-h-60 md:max-h-100 p-2 border">
+                            <div className="w-full rounded-2xl border-[#1493ac] bg-white/5 flex justify-between md:w-[80%] lg:w-[65%] min-h-40 max-h-60 md:max-h-100 p-2 border">
                                 <Textarea className="resize-none border-0 md:text-lg focus:ring-0" placeholder="Ask anything..."
                                     value={prompt}
                                     onChange={(e) => { setPrompt(e.target.value) }}
@@ -72,7 +72,7 @@ const PromptInput: React.FC = () => {
                                 whileTap={{ scale: 0.9 }}
                                 transition={{ type: "spring" }}
                             >
-                                <Button className="px-10 py-5 bg-black hover:bg-[#1493ac] border border-[#1493ac] text-lg font-semibold cursor-pointer transition duration-300 ease-in-out"
+                                <Button className="px-10 py-5 bg-white/5 hover:bg-[#1493ac] border border-[#1493ac] text-lg font-semibold cursor-pointer transition duration-300 ease-in-out"
                                     onClick={() => getExplanation()}
                                     disabled={(prompt === '' || isFetching)}
                                 >
